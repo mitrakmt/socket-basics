0c1e05f4e600c97d371b36185a3fe66acb5ce61b
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-24 09:08:01 -0700

    Add current users command

--- server.js
+++ server.js
@@ -7,9 +7,31 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-var clientInfo = {
+var clientInfo = {};
 
-};
+// Send current users to provided socket
+function sendCurrentUsers(socket) {
+  var info = clientInfo[socket.id];
+  var users = [];
+
+  if (typeof info === 'undefined') {
+    return;
+  }
+
+  Object.keys(clientInfo).forEach(function (socketId) {
+    var userInfo = clientInfo[socketId];
+
+    if (info.room === userInfo.room) {
+      users.push(userInfo.name);
+    }
+  });
+
+  socket.emit('message', {
+    name: 'System',
+    text: 'Current Users: ' + users.join(', '),
+    timestamp: moment().valueOf()
+  });
+}
 
 io.on('connection', function (socket) {
   console.log("User connected via socket.io");
@@ -38,8 +60,12 @@ io.on('connection', function (socket) {
   });
 
   socket.on('message', function (message) {
-    message.timestamp = moment().valueOf();
-    io.to(clientInfo[socket.id].room).emit('message', message);
+    if (message.text === '@currentUsers') {
+      sendCurrentUsers(socket);
+    } else {
+      message.timestamp = moment().valueOf();
+      io.to(clientInfo[socket.id].room).emit('message', message);
+    }
   });
 
   socket.emit('message', {
