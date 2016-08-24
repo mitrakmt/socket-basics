ca65cb513092e1a31a9c38fa058ce3845ae1d0bd
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-23 19:13:43 -0700

    Added query paramter functionality and name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,30 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+
+    return undefined;
+}
 
 socket.on('connect', function() {
   console.log("connected to Socket.io server");
 });
 
 socket.on('message', function (message) {
-  jQuery('.messages').append('<p>' + message.text + '</p>')
+  var momentTimestamp = moment.utc(message.timestamp);
+  var $message = $('.messages');
+
+  $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+  $message.append('<p>' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -17,6 +36,7 @@ $form.on('submit', function (event) {
   var $message = $form.find('input[name=message]');
 
   socket.emit('message', {
+    name: name,
     text: $message.val()
   });
 
